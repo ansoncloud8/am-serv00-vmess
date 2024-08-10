73d75422a3b051be2f1f6958c006643240ff93ff
AM科技 <137372060+ansoncloud8@users.noreply.github.com>
2024-08-10 17:59:35 +0800

    Update install_serv00_vmess.sh

--- install_serv00_vmess.sh
+++ install_serv00_vmess.sh
@@ -191,6 +191,23 @@ generate_config() {
     {
       "type": "dns",
       "tag": "dns-out"
+    },
+    {
+      "type": "wireguard",
+      "tag": "wireguard-out",
+      "server": "162.159.195.100",
+      "server_port": 4500,
+      "local_address": [
+        "172.16.0.2/32",
+        "2606:4700:110:83c7:b31f:5858:b3a8:c6b1/128"
+      ],
+      "private_key": "mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=",
+      "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+      "reserved": [
+        26,
+        21,
+        228
+      ]
     }
   ],
   "route": {
